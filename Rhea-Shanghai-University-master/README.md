d463236beb0f0b3952c61b961e20632aa0ca53b8
500kg <2426958134@qq.com>
2018-01-31 13:15:31 +0800

    18/01/31

---
+++ Rhea-Shanghai-University-master/README.md
@@ -0,0 +1,47 @@
+# Rhea
+上海大学刷课助手 -- chrome插件(Chrome extension)
+
+- [实现功能](#实现功能)
+- [如何安装](#如何安装)
+- [使用说明](#使用说明)
+- [实现原理](#实现原理)
+- [致谢](#致谢)
+- [License](#license)
+
+## 实现功能
+- 模拟点击提交按钮自动刷课
+- 教学评估辅助选中
+- 课程表简单美化
+
+## 如何安装
+- __方法一__
+	- 点击右上角的Download ZIP，解压的到任意的地方
+	- 打开chrome的扩展程序，选中“开发者模式”，然后点击“加载已解压的扩展程序”按钮，选择你解压得到的目录
+	- 如果看到chrome右上角多了![icon](icons/Aristotle_24.png)图标，就OK了!
+- __方法二__
+	- 如果对程序放心的话，可以直接下载[crx文件](https://github.com/alfredcai/Rhea-Shanghai-University/releases)
+	- 打开chrome的扩展程序，将文件拖入就可以了！	
+- 安装本软件后，每次打开浏览器会建议关闭开发者模式安装的插件，这时点取消可以保持本插件继续运行
+- 如果想__暂时停用__，请在扩展程序管理页面取消“已启用”前的对勾即可
+- 如果想__卸载__，请在扩展程序管理页面点击垃圾桶图标即可
+
+## 使用说明
+- 已经在页面上添加了相应的按钮，根据名字选择就好
+- 刷课时想看到更多详细信息，可以打开浏览器控制台查看
+- 当前设定为每10秒点击一次提交按钮
+- __关于停止选课__
+	- 点击浏览器右上角的插件图标![icon](icons/Aristotle_24.png)，弹出的页面有___停止选课的按钮___
+
+## 实现原理
+- 调用JavaScript的setTimeOut方法点击提交按钮
+- 关于提交表单session并没有刷新的问题，只是粗糙的刷新的整个页面，利用插件支持的本地存储，将保存的课程号再次填入继续提交
+- 其他就是一些jQuery控制页面元素的调用没什么可说的
+
+## 致谢
+- 图标来自于[ICONFINDER](https://www.iconfinder.com/)的[beard_face_greece_greek_philosopher_platonic_scientist](https://www.iconfinder.com/icons/362069/aristotle_avatar_beard_face_greece_greek_philosopher_platonic_scientist_icon)
+- [jQuery](http://jquery.com)
+- [Bootstrap](http://getbootstrap.com)
+
+## License
+
+[MIT licensed](LICENSE)
---
+++ Rhea-Shanghai-University-master/scripts/selectCourse.js
@@ -0,0 +1,111 @@
+/* @author Alfred Cai
+ * Licensed under the MIT License
+ */
+
+var Btn = function (button_name, class_name) {
+	return $("<input type='button'>")
+		.val(button_name)
+		.addClass(class_name)
+		.css('width', '100px')
+		.css('background-color', 'navajowhite')
+		.css('margin', '0px 20px')
+};
+var Lesson = function (input_classNum, input_teaNum) {
+	return {
+		classNum: input_classNum,
+		teaNum: input_teaNum
+	}
+};
+
+$(function () {
+	var $btn_submit = Btn('开始自动刷课', 'btn-interval-submit');
+	$('#FastInputAction').before($btn_submit);
+
+	$('.btn-interval-submit').click(function () {
+		if (initAndSave()) {
+			refreshPage();
+			submit_start();
+		}
+	});
+
+	$('.btn-refresh').click(function () {
+		location.reload();
+	});
+});
+
+function initAndSave() {
+	chrome.storage.local.set({ 'selected': [] }, function () {
+		console.log('clear storage');
+	});
+	var ListCourse = [];
+	var count = 0;
+	for (var i = 0; i < 6; i++) {
+		var cln = $('#CID' + i).val();
+		var tn = $('#TNo' + i).val();
+		if (cln && tn) {
+			var a = Lesson(cln, tn);
+			console.log('get course%s:%s,%s', i + 1, a.classNum, a.teaNum);
+			ListCourse.push(a);
+			count += 1;
+		}
+	}
+	if (count > 0) {
+		chrome.storage.local.set({ 'selected': ListCourse }, function () {
+			console.log('init and save ListCourse');
+		});
+		return 1;
+	} else {
+		alert('at least choose one lesson');
+		return 0;
+	}
+};
+
+var submit_time = 0, refresh_time = 0, submit_count = 1;
+
+function submit_start() {
+	chrome.storage.local.get('selected', function (items) {
+		var ListCourse = items.selected;
+		if (ListCourse) {
+			if (ListCourse.length > 0) {
+				for (var i in ListCourse) {
+					var a = ListCourse[i];
+					$('#CID' + i).val(a.classNum);
+					$('#TNo' + i).val(a.teaNum);
+					console.log('submit course%s:%s,%s', i + 1, a.classNum, a.teaNum);
+				}
+				$('#FastInputAction').trigger('click');
+				submit_time = setTimeout("submit_start()", 10000);
+				console.log('setTimeout id of submit:%s, the %s time to submit on %s', submit_time, submit_count++, new Date());
+			} else {
+				stop();
+			}
+		}
+	});
+};
+
+function refreshPage() {
+	refresh_time = setTimeout(function () {
+		location.reload();
+	}, 10 * 60 * 1000);
+	console.log('setTimeout id of refresh:%s on %s', refresh_time, new Date());
+}
+
+function stop() {
+	if (submit_time) {
+		clearTimeout(submit_time);
+		console.log('clear timeout of submit:%s', submit_time);
+
+	}
+	if (refresh_time) {
+		clearTimeout(refresh_time);
+		console.log('clear timeout of refresh:%s', refresh_time);
+	}
+}
+
+chrome.storage.local.get('selected', function (items) {
+	var ListCourse = items.selected;
+	if (ListCourse.length > 0) {
+		refreshPage();
+		submit_start();
+	}
+});
